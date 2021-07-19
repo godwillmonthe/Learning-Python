1c94494de796fb76cc90e890097d77276deab577
Godwill Monthe <oletilemonthe@gmail.com>
2021-07-19 13:34:52 +0200

    lesson 1: How code is executed

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+#LESSON 1
+
+####How python code gets executed
+
+In this python tutorial we'll learn how python code is executed.
--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+# How python code is executed
+
+print('Godwill Monthe')
+print('o----')
+print(' ||||')
+
+# when this code is ran, the terminal will print the statements above line by line
